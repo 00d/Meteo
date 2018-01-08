0d93c40d239df5f9b68f3ddc1586400342e4321e
John Britzke <jb@johns-macbook>
2018-01-08 04:12:26 -0800

    Initial Commit

---
+++ Meteo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meteo
+//
+//  Created by John Britzke on 1/8/18.
+//  Copyright © 2018 Thaumatech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
